485e618dc4544b197272ee9d1777f5f38f6bb53e
katawong0@gmail.com <sotgame123>
2020-09-20 16:59:32 +0700

    Updates

---
+++ precache-manifest.fa7fad2ee1b2af3b8cb39e5bd318466d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad188a5a4825a76071f57beca1f8772",
+    "url": "/covid-testReact/index.html"
+  },
+  {
+    "revision": "789f8a12f0d255d1c58f",
+    "url": "/covid-testReact/static/css/main.235d9968.chunk.css"
+  },
+  {
+    "revision": "8ed2bc3fa86c6309176b",
+    "url": "/covid-testReact/static/js/2.f697a951.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-testReact/static/js/2.f697a951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "789f8a12f0d255d1c58f",
+    "url": "/covid-testReact/static/js/main.fb1ae156.chunk.js"
+  },
+  {
+    "revision": "d5f96f468b16ceb9e083",
+    "url": "/covid-testReact/static/js/runtime-main.28d5be8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-testReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
